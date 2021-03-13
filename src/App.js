954835c308794058bbc0278f01578422abc62541
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-03-13 17:30:46 +0530

    feed-input-done

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
 				<Feeds />
 				{/* widgets */}
 			</div>
+			
 		</div>
 	);
 }
--- src/components/Feeds/Feeds.js
+++ src/components/Feeds/Feeds.js
@@ -2,20 +2,20 @@ import CreateIcon from '@material-ui/icons/Create';
 import React from 'react';
 import './Feeds.css';
 const Feeds = () => {
-    return (
-        <div className='feed' >
-            <div className="feed__inputcontainer">
-                <CreateIcon />
-                <div className="feed__input">
-                    <form action="">
-                        <input type="text"/>
-                        <button type='submit' >Send</button>
-                    </form>
-                </div>
-            </div>
-            
-        </div>
-    )
-}
+	return (
+		<div className='feed'>
+			<div className='feed__inputcontainer'>
+				<div className='feed__input'>
+					<CreateIcon />
+					<form action=''>
+						<input type='text' />
+						<button type='submit'>Send</button>
+                        
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Feeds
+export default Feeds;
