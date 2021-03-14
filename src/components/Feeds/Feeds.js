39378596f838f1a1f4792bb084b5fd47f6d9135d
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-03-14 12:31:39 +0530

    feeds-done--moving to login func

--- src/components/Feeds/Feeds.js
+++ src/components/Feeds/Feeds.js
@@ -3,33 +3,59 @@ import CreateIcon from '@material-ui/icons/Create';
 import EventNoteIcon from '@material-ui/icons/EventNote';
 import ImageIcon from '@material-ui/icons/Image';
 import SubscriptionsIcons from '@material-ui/icons/Subscriptions';
-import React,{useState} from 'react';
+import firebase from 'firebase';
+import React, { useEffect, useState } from 'react';
+import { db } from '../Firebase/Firebase';
 import './Feeds.css';
 import InputOptions from './InputOptions/InputOption';
 import Posts from './Posts/Posts';
 
 const Feeds = () => {
 	const [posts, setPosts] = useState([]);
+	const [input, setInput] = useState('');
 
-	
+	///Updating db with realtime event listener using useEffect//
+	useEffect(() => {
+		db.collection('posts')
+			.orderBy('timestamp', 'desc')
+			.onSnapshot((snapshot) =>
+				setPosts(
+					snapshot.docs.map((doc) => ({
+						id: doc.id,
+						data: doc.data(),
+					}))
+				)
+			);
+	}, []);
 
-
-	const sendPost = (e)=>{
+	const sendPost = (e) => {
 		e.preventDefault();
 
-		setPosts([...posts],posts);
-	}
+		db.collection('posts').add({
+			name: 'Aravind Murugan',
+			description: 'this is a test',
+			message: input,
+			photoUrl: '',
+			timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+		});
 
-
+		setInput('');
+	};
 
 	return (
 		<div className='feed'>
 			<div className='feed__inputcontainer'>
 				<div className='feed__input'>
 					<CreateIcon />
-					<form action=''>
-						<input type='text' />
-						<button type='submit' onClick={sendPost} >Send</button>
+					<form>
+						<input
+							value={input}
+							onChange={(e) => setInput(e.target.value)}
+							type='text'
+						/>
+						<button type='submit' onClick={sendPost}>
+							Send
+						</button>
 					</form>
 				</div>
 				<div className='feed__inputoptions'>
@@ -48,12 +74,15 @@ const Feeds = () => {
 				</div>
 			</div>
 
-			<Posts
-				name='Aravind Murugan'
-				description='test description'
-				message='this is a test message'
-				photoUrl=''
-			/>
+			{posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+				<Posts
+					key={id}
+					name={name}
+					description={description}
+					message={message}
+					photoUrl={photoUrl}
+				/>
+			))}
 		</div>
 	);
 };
